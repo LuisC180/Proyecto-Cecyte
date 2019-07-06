6b11e56713757eaa8e228b6df7a61edbba34b14f
LuisC180 <zolecito_88@hotmail.com>
2019-07-05 19:56:29 -0500

    Se agregó Agregar docente.php y la base

---
+++ AgregarDocente.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="es">
+
+  <title>Cedula de control incidencias</title>
+ 
+ <center><h2 class="card-title">ALTA PERSONAL</h2></center><br><br>
+ <div class="row">       
+        <div class="col-md">        
+            <div class="card border-success">
+              <div class="card-body">
+                <center><h3 class="card-title">INFORMACIÓN PERSONAL</h3></center>
+           
+           <div class="row">
+  <div class="col-md-4">
+     <STRONG>Apellido Paterno: </STRONG><br>
+    <input type="text" class="form-control uppercase" name="txtapellidoP">
+  </div>
+
+  <div class="col-md-4">
+     <STRONG>Apellido Materno: </STRONG><br>
+    <input type="text" class="form-control uppercase" name="txtapellidoM">
+  </div>
+
+    
+    <div class="col-md-4">
+     <STRONG>Nombre(s):</STRONG><br>
+    <input type="text"class="form-control uppercase" name="txtnombre">
+  </div>
+</div>
+<br>
+
+<div class="row">
+  <div class="col-md-4">
+     <STRONG>RFC: </STRONG><br>
+      <input type="text" class="form-control uppercase" name="txtRFC">
+    </div>
+  <div class="col-md-4">
+   <STRONG>CURP: </STRONG><br>
+    <input type="text" class="form-control uppercase" name="txtCURP">
+     </div>
+  <div class="col-sm-4">
+    <STRONG>Fecha de nacimiento:</STRONG><br>
+      <input class="form-control" type="date" value=""  name="datefecha">
+      </div>
+    </div>
+    <br>
+  
+  <div class="row">
+    <div class="col-md-4">
+      <STRONG>Tipo de sangre: </STRONG><br>
+        <input type="text" class="form-control uppercase" name="txtTipoS">
+      </div>
+  <div class="col-md-4">
+    <STRONG>Lugar de nacimiento: </STRONG><br>
+      <input type="text" class="form-control uppercase" name="txtLugarN">
+     </div>
+  <div class="col-md-4">
+    <STRONG>No.IMSS:</STRONG><br>
+      <input type="text"class="form-control uppercase" name="txtIMSS">
+    </div>
+  </div>
+  <br>
+  
+  <div class="row">
+    <div class="col-md-4">
+     <STRONG>Correo electronico: </STRONG><br>
+       <input type="text" class="form-control uppercase" name="txtCorreoEle">
+  </div>
+
+  <div class="col-md-4">
+    <STRONG>Telefono de casa: </STRONG><br>
+      <input type="text" class="form-control uppercase" name="txtTelefonoCasa">
+    </div>
+
+    <div class="col-md-4">
+     <STRONG>Telefono celular:</STRONG><br>
+      <input type="text"class="form-control uppercase" name="txtTelefonoCel">
+    </div>
+  </div>
+  <br>
+</div>
+<br>
+  </div>
+    </div>
+      </div>
+  <br>
+  <br>
+
+<div class="row">      
+ <div class="col-md">        
+  <div class="card border-success">
+   <div class="card-body">
+     <center><h3 class="card-title">DIRECCIÓN</h3></center>
+       <br>
+     
+  <div class="row">
+    <div class="col-md-4">
+     <STRONG>Colonia o Comunidad: </STRONG><br>
+       <input type="text" class="form-control uppercase" name="txtColonia">
+     </div>
+
+  <div class="col-md-4">
+   <STRONG>Calle:</STRONG><br>
+    <input type="text" class="form-control uppercase" name="txtCalle">
+  </div>
+
+   <div class="col-md-4">
+     <STRONG>Numero:</STRONG><br>
+    <input type="text"class="form-control uppercase" name="txtNum">
+  </div>
+</div>
+<br>
+<div class="row">
+    <div class="col-md-4">
+     <STRONG>Codigo Postal: </STRONG><br>
+    <input type="text" class="form-control uppercase" name="txtColonia">
+  </div>
+
+  <div class="col-md-4">
+     <STRONG>Municipio:</STRONG><br>
+    <input type="text" class="form-control uppercase" name="txtCalle">
+  </div>
+
+   <div class="col-md-4">
+     <STRONG>Estado:</STRONG><br>
+    <input type="text"class="form-control uppercase" name="txtNum">
+  </div>
+</div>
+<br>
+  </div>
+
+    
+    </div>
+  </div>
+</div>
+<br>
+  <br>
+<div class="row">      
+ <div class="col-md">        
+  <div class="card border-success">
+   <div class="card-body">
+     <center><h3 class="card-title">INFORMACIÓN LABORAL</h3></center>
+     <br>
+     
+  <div class="row">
+    <div class="col-md-4">
+     <STRONG>No.Empleado: </STRONG><br>
+    <input type="text" class="form-control uppercase" name="txtNumEmp">
+  </div>
+
+ <div class="col-sm-4">
+    <STRONG>Fecha de ingreso:</STRONG><br>
+      <input class="form-control" type="date" value=""  name="datefecha">
+      </div>
+
+  <div class="col-md-4">
+     <STRONG>Estatus:</STRONG>
+    <select class="form-control" name="cmbEstatus">
+    <option></option>
+    <option>Empleado Base</option>
+    <option>Empleado Confianza</option>
+    <option>Empleado Interino</option>
+    </select name=concepto>
+  </div>
+</div>
+<br>
+
+
+<div class="row">
+    <div class="col-md-4">
+     <STRONG>Departamento: </STRONG><br>
+    <input type="text" class="form-control uppercase" name="txtDepartamento">
+  </div>
+
+  <div class="col-md-4">
+     <STRONG>Puesto:</STRONG><br>
+    <input type="text" class="form-control uppercase" name="txtPuesto">
+  </div>
+
+   <div class="col-md-4">
+     <STRONG>Horas:</STRONG><br>
+    <input type="text"class="form-control uppercase" name="txtHoras">
+  </div>
+</div>
+<br>
+     <a  href="#" class="btn btn-success">GUARDAR</a>
+  </div>
+
+    
+    </div>
+  </div>
+</div>
+</body>
+</html>
